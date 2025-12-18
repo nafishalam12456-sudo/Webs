7f3de166e2a052075de99b1ecfb318b453a28130
nafishalam12456-sudo <nafishalam12456@gmail.com>
2025-12-18 19:48:40 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,93 @@
-<html style="height:100%"><head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--- made with love by arfan rizki ramadhan --->
+
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title> 403 Forbidden
-</title></head>
-<body style="color: #444; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:100%; background-color: #fff;">
-<div class="embed-responsive embed-responsive-16by9" style="height:100vh">
-<div style="height:auto; min-height:100%; ">     <div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;">
-        <h1 style="margin:0; font-size:150px; line-height:150px; font-weight:bold;">403</h1>
-<h2 style="margin-top:20px;font-size: 30px;">Forbidden
-</h2>
-<p>Access to this resource on the server is denied!</p>
-<p>Telegram @EXP_VIP_H4CKER</p>
-</div></div>
-</body></html>
+<meta property="og:title" content="arpanrizki">
+<meta name="description" content="This website made by Arfan Rizki Ramadhan">
+<meta property="og:description" content="This website made by Arfan Rizki Ramadhan">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="arpanrizki">
+<meta property="og:type" content="website">
+<meta name="copyright"content="arpanrizki">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="This website made by Arfan Rizki Ramadhan">
+<title>CHEIF YT STORE</title>
+<link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Staatliches&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="https://images.vexels.com/media/users/3/143495/isolated/preview/6b80b9965b1ec4d47c31d7eccf8ce4b0-yellow-lightning-bolt-icon-by-vexels.png">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+
+<style>
+body {
+ background: url(https://user-images.githubusercontent.com/101210220/209577372-d25a50cc-9ab8-431f-8c42-b4abc3b2255b.jpg) no-repeat center center;
+ background-size: cover;
+ margin: 0px;
+ padding: 0px;
+}
+.bungkusan {
+ background: rgba(0, 0, 0, 0.4);
+ width: 100%;
+ height: 100%;
+}
+.container {
+ max-width: 400px;
+ height: auto;
+ margin-left: auto;
+ margin-right: auto;
+ display: block;
+}
+.tulisan {
+ padding-top: 50%;
+ color: #fff;
+ font-size: 30px;
+ font-family: 'Bubblegum Sans', cursive;
+ text-align: center;
+}
+.tulisan i {
+ color: #fff;
+ margin-left: auto;
+ margin-right: auto;
+ margin-bottom: 20px;
+ display: block;
+}
+button {
+ background: #fff;
+ width: auto;
+ height: auto;
+ margin-top: 15px;
+ margin-left: auto;
+ margin-right: auto;
+ padding: 8px;
+ padding-left: 12px;
+ padding-right: 12px;
+ color: #000;
+ font-size: 15px;
+ font-family: 'Bubblegum Sans', cursive;
+ text-align: center;
+ border: none;
+ border-radius: 40px;
+ outline: none;
+ display: block;
+}
+</style>
+
+<div class="bungkusan">
+
+<div class="container">
+<div class="tulisan">
+<i class="zmdi zmdi-settings zmdi-hc-4x zmdi-hc-spin"></i>
+are you looking for Phishing Script?
+</div>
+<button type="button" onmousedown="tutup.play();" style="background: url(stark/img/yes_selow.png) no-repeat center center; background-size: 100% 100%;" onclick="location.href='https://telegram.me/CHEIF_YT';">CLICK TO CONTACT ME</button>
+</div>
+
+</div>
+
+</body>
+</html>
+
+<!--- made with love by arfan rizki ramadhan --->
